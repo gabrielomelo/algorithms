3f1907ddf070dbcc7790d43583182e4a62ad3432
gabrielomelo <dev.gabrielomelo@gmail.com>
2018-08-10 16:36:16 -0300

    Texto re-identado

--- cv/haar/facial-recognition.py
+++ cv/haar/facial-recognition.py
@@ -17,18 +17,18 @@ cascade = cv.CascadeClassifier(haar_path)
 camera = cv.VideoCapture(0)
 
 while True:
-	
-	img_value, img = camera.read()
-	img = cv.flip(img, 1)
-	gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-	
-	#Classificando em cascata, usando o haar
-	positions = cascade.detectMultiScale(gray_img, 1.3, 5)
-	
-	for (x, y, w, h) in positions:
-		cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-        cv.imshow("Source", img)
-
-	if cv.waitKey(1) == 27:
-	    break
+    img_value, img = camera.read()
+    img = cv.flip(img, 1)
+    gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    #Classificando em cascata, usando o haar
+    
+    positions = cascade.detectMultiScale(gray_img, 1.3, 5)
+    
+    for (x, y, w, h) in positions:
+        cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    cv.imshow("Source", img)
+    if cv.waitKey(1) == 27:
+        break
